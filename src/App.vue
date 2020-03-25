2823ba4c751c253fe3a8dfacdab39b56b79408e9
x3malmusic <x3malmusic@gmail.com>
2020-03-25 18:08:17 +0200

    Why isn't this working!? Is there something missing!? more blood must be shed!?

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,32 @@
     <!--    your awesome traffic-->
     <section class="awesome-traffic-section">
       <Header />
-      <div class="awesome-traffic-section-content">
+      <div class="section awesome-traffic-section-content">
         <h1>Your awesome traffic permit consultant.</h1>
         <Btn title="get started" :arrow="true" />
       </div>
-      <img src="./assets/Home/mobel.png" alt="" class="traffic-car"/>
+      <img src="./assets/Home/mobel.png" alt="" class="traffic-car" />
     </section>
     <!--    your awesome traffic end-->
+
+    <!--   sections -->
+    <section class="sections-section section">
+      <section-component
+        v-for="section in sections"
+        :title="section.title"
+        :text="section.text"
+        :warning="section.warning"
+        :img="section.img"
+      />
+    </section>
+    <!--   sections end-->
   </div>
 </template>
 
 <script>
 import Header from "./components/Header/Header.vue";
 import Btn from "./components/Button/Btn.vue";
+import SectionComponent from "./components/SectionComponent/SectionComponent.vue";
 
 import "./main.css";
 
@@ -24,7 +37,20 @@ export default {
   components: {
     Header,
     Btn,
+    SectionComponent,
   },
+  data: () => ({
+    sections: [
+      {
+        title: "about us",
+        text:
+          "The occupational traffic permit is one of the most important things in the company when carrying out freight transport. In fact, it is a prerequisite for doing business traffic at all.",
+        warning:
+          "How do you do when you need to obtain a commercial traffic permit for freight transport to your business?",
+        img: "../../assets/Home/man.png",
+      },
+    ],
+  }),
 };
 </script>
 
---
+++ src/components/SectionComponent/SectionComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="section-wrapper">
+    <div class="section-content">
+      <h3>{{ title }}</h3>
+      <p>{{ text }}</p>
+      <div>
+        <h3>{{ warning }}</h3>
+      </div>
+    </div>
+    <img :src="getImg" alt="" />
+  </div>
+</template>
+
+<script>
+import "./sectioncomponent.css";
+
+export default {
+  props: {
+    title: String,
+    text: String,
+    warning: String,
+    img: String,
+  },
+  computed: {
+    getImg() {
+      return require(this.img);
+    },
+  },
+  name: "SectionComponent",
+};
+</script>
